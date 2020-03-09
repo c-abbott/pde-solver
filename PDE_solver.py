8bc94fe370bd04c39689d65bcc2fd7d7839284bf
c-abbott <callum.abbott@outlook.com>
2020-03-09 16:14:56 +0000

    Implemented animation.

--- PDE_solver.py
+++ PDE_solver.py
@@ -13,7 +13,7 @@ class CahnHilliard(object):
         self.build_matrices()
 
     def build_matrices(self):
-        self.phi = np.random.choice(a=[0, 1], size=self.size)
+        self.phi = np.random.choice(a=[-1, 1], size=self.size)
         self.mu = np.zeros(self.size)
     
     def disc_laplacian(self, field, position):
@@ -34,16 +34,58 @@ class CahnHilliard(object):
     
     def euler_update(self, position):
         i, j = position
-        next_step = self.mu[i, j] + (self.mob*self.dt/self.dx**2)
-        * (self.mu[self.pbc((i-1, j))] + self.mu[self.pbc((i+1, j))]
+        next_step = self.mu[i, j] + (self.mob*self.dt/self.dx**2)*(self.mu[self.pbc((i-1, j))] + self.mu[self.pbc((i+1, j))]
            + self.mu[self.pbc((i, j-1))] + self.mu[self.pbc((i, j+1))]
            - 4 * self.mu[i, j]
            )
         return (next_step)
-   
+    
+    def parallel_update(self, function):
+        new_state = np.zeros(self.size)
+        for i in range(self.size[0]):
+            for j in range(self.size[1]):
+                new_state[i, j] = function((i, j))
+        return(new_state)
+    
+    def update_phi(self):
+        new_state = np.zeros(self.size)
+        for i in range(self.size[0]):
+            for j in range(self.size[1]):
+                new_state[i, j] = self.euler_update((i, j))
+        self.phi = new_state
+    
+    def update_mu(self):
+        new_state = np.zeros(self.size)
+        for i in range(self.size[0]):
+            for j in range(self.size[1]):
+                new_state[i, j] = self.calc_mu((i, j))
+        self.mu = new_state
+
+    def update_cahn_hilliard(self):
+        self.update_mu()
+        self.update_phi()
+
     def pbc(self, indices):
         """
             Applies periodic boundary conditions (pbc) to a
             2D lattice.
         """
         return(indices[0] % self.size[0], indices[1] % self.size[1])
+    
+    def animate(self, *args):
+        for i in range(self.it_per_frame):
+            self.update_cahn_hilliard()
+        self.image.set_array(self.phi)
+        return self.image,
+    
+    def run_animation(self, iterations, it_per_frame):
+        """
+            Used in partnership with the tester file
+            to run the simulation.
+        """
+        self.it_per_frame = it_per_frame
+        self.figure = plt.figure()
+        self.image = plt.imshow(self.phi, cmap='jet', animated=True)
+        self.animation = animation.FuncAnimation(
+            self.figure, self.animate, repeat=False, frames=iterations, interval=50, blit=True)
+        plt.show()
